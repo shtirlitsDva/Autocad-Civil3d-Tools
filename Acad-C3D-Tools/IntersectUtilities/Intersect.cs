371f62b3449a3c105576f452d6f8ae8137f0e8d5
Michail Golubjev <xopkop@gmail.com>
2019-05-07 00:44:52 +0200

    Intersection of polyline with all lines achieved.

--- Acad-C3D-Tools/IntersectUtilities/Intersect.cs
+++ Acad-C3D-Tools/IntersectUtilities/Intersect.cs
@@ -3,11 +3,164 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Aec.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Civil.DatabaseServices.Styles;
+
+using oid = Autodesk.AutoCAD.DatabaseServices.ObjectId;
 
 namespace IntersectUtilities
 {
-    public class Intersect
+    /// <summary>
+    /// Class for intersection tools.
+    /// </summary>
+    public class Intersect : IExtensionApplication
     {
-        string test = "test";
+        #region IExtensionApplication members
+        public void Initialize()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            doc.Editor.WriteMessage("\n-> Command: intut");
+        }
+
+        public void Terminate()
+        {
+        }
+        #endregion
+
+        #region Global objects used throughout the application
+        private DocumentCollection docCol = null;
+        private CivilDocument doc = null;
+        private Database db = null;
+        private Editor editor = null;
+        #endregion
+
+        /// <summary>
+        /// Finds all intersections between a selected polyline and all lines.
+        /// Creates a point object at the intersection.
+        /// </summary>
+        [CommandMethod("intut")]
+        public void intersectutilities()
+        {
+            docCol = Application.DocumentManager;
+            db = docCol.MdiActiveDocument.Database;
+            editor = docCol.MdiActiveDocument.Editor;
+            doc = Autodesk.Civil.ApplicationServices.CivilApplication.ActiveDocument;
+
+            using (Transaction tx = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    PromptEntityOptions promptEntityOptions1 = new PromptEntityOptions("\n Select a polyline : ");
+                    promptEntityOptions1.SetRejectMessage("\n Not a polyline");
+                    promptEntityOptions1.AddAllowedClass(typeof(Polyline), true);
+                    PromptEntityResult entity1 = editor.GetEntity(promptEntityOptions1);
+                    if (((PromptResult)entity1).Status != PromptStatus.OK) return;
+                    Autodesk.AutoCAD.DatabaseServices.ObjectId plObjId = entity1.ObjectId;
+                    Polyline polyline = tx.GetObject(plObjId, OpenMode.ForRead, false) as Polyline;
+
+
+                    List<Line> lines = db.ListOfType<Line>(tx);
+                    List<Point3d> p3dList = new List<Point3d>();
+
+                    foreach (Line line in lines)
+                    {
+                        using (Point3dCollection p3dcol = new Point3dCollection())
+                        {
+                            polyline.IntersectWith(line, 0, p3dcol, new IntPtr(0), new IntPtr(0));
+
+                            foreach (Point3d p3d in p3dcol) p3dList.Add(p3d);
+                        }
+                    }
+
+                    // Get the block table for the current database
+                    var blockTable = (BlockTable)tx.GetObject(db.BlockTableId, OpenMode.ForRead);
+
+                    // Get the model space block table record
+                    var modelSpace = (BlockTableRecord)tx.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+
+                    foreach (Point3d p3d in p3dList)
+                    {
+                        using (DBPoint acPoint = new DBPoint(p3d))
+                        {
+                            modelSpace.AppendEntity(acPoint);
+                            tx.AddNewlyCreatedDBObject(acPoint, true);
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    editor.WriteMessage("\n" + ex.Message);
+                }
+                tx.Commit();
+            }
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static void ForEach<T>(this Database database, Action<T> action, Transaction tr) where T : Autodesk.AutoCAD.DatabaseServices.Entity
+        {
+            //using (var tr = database.TransactionManager.StartTransaction())
+            //{
+            // Get the block table for the current database
+            var blockTable = (BlockTable)tr.GetObject(database.BlockTableId, OpenMode.ForRead);
+
+            // Get the model space block table record
+            var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
+
+            RXClass theClass = RXObject.GetClass(typeof(T));
+
+            // Loop through the entities in model space
+            foreach (oid objectId in modelSpace)
+            {
+                // Look for entities of the correct type
+                if (objectId.ObjectClass.IsDerivedFrom(theClass))
+                {
+                    var entity = (T)tr.GetObject(objectId, OpenMode.ForRead);
+                    action(entity);
+                }
+            }
+            //tr.Commit();
+            //}
+        }
+
+        public static List<T> ListOfType<T>(this Database database, Transaction tr) where T : Autodesk.AutoCAD.DatabaseServices.Entity
+        {
+            //using (var tr = database.TransactionManager.StartTransaction())
+            //{
+
+            //Init the list of the objects
+            List<T> objs = new List<T>();
+
+            // Get the block table for the current database
+            var blockTable = (BlockTable)tr.GetObject(database.BlockTableId, OpenMode.ForRead);
+
+            // Get the model space block table record
+            var modelSpace = (BlockTableRecord)tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
+
+            RXClass theClass = RXObject.GetClass(typeof(T));
+
+            // Loop through the entities in model space
+            foreach (oid objectId in modelSpace)
+            {
+                // Look for entities of the correct type
+                if (objectId.ObjectClass.IsDerivedFrom(theClass))
+                {
+                    var entity = (T)tr.GetObject(objectId, OpenMode.ForRead);
+                    objs.Add(entity);
+                }
+            }
+            return objs;
+            //tr.Commit();
+            //}
+        }
     }
 }
