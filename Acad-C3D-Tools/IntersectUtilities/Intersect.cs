d15cce658a40800689661a14f2255f01bb216d46
Michail Golubjev <xopkop@gmail.com>
2019-05-06 21:09:58 +0200

    Create Intersect project

---
+++ Acad-C3D-Tools/IntersectUtilities/Intersect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectUtilities
+{
+    public class Intersect
+    {
+    }
+}
