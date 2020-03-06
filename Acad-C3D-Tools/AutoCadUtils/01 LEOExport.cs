d472d59835c209d0f9f2077109ae7fe932d6cac0
Michail Golubjev <xopkop@gmail.com>
2020-03-06 22:41:12 +0100

    Start new Autocad utility

---
+++ Acad-C3D-Tools/AutoCadUtils/01 LEOExport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AutoCadUtils
+{
+    public class ExportLeoComponentsInRooms : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            doc.Editor.WriteMessage("\n-> Export LEO components to EXCEL: EXPORTLEO");
+        }
+
+        public void Terminate()
+        {
+        }
+
+        [CommandMethod("exportleo")]
+        public void exportleo()
+        {
+
+        }
+    }
+}
