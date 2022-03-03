b8dcc9469178d87900b9f4ae04cc3bcd3a425b8c
Michail Golubjev <xopkop@gmail.com>
2022-03-03 22:51:41 +0100

    Add palette project

--- Acad-C3D-Tools/DriPaletteSet/TwinPalette.Designer.cs
+++ Acad-C3D-Tools/DriPaletteSet/TwinPalette.Designer.cs
@@ -33,6 +33,9 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -65,13 +68,46 @@
             this.checkBox3.Text = "DN 32";
             this.checkBox3.UseVisualStyleBackColor = true;
             // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(43, 215);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(104, 29);
+            this.checkBox4.TabIndex = 0;
+            this.checkBox4.Text = "DN 40";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(43, 274);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(104, 29);
+            this.checkBox5.TabIndex = 0;
+            this.checkBox5.Text = "DN 50";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(43, 333);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(104, 29);
+            this.checkBox6.TabIndex = 0;
+            this.checkBox6.Text = "DN 65";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
             // TwinPalette
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Controls.Add(this.checkBox6);
             this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox5);
             this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.checkBox1);
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "TwinPalette";
@@ -86,5 +122,8 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
     }
 }
