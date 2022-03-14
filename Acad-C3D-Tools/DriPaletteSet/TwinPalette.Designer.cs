65cd3462efd540c9eb5488027ad4ea65ab5f47bd
Michail Golubjev <xopkop@gmail.com>
2022-03-14 22:41:16 +0100

    cannot work... cannot think... this is madness....

--- Acad-C3D-Tools/DriPaletteSet/TwinPalette.Designer.cs
+++ Acad-C3D-Tools/DriPaletteSet/TwinPalette.Designer.cs
@@ -36,84 +36,181 @@
             this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.checkBox5 = new System.Windows.Forms.CheckBox();
             this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(43, 42);
+            this.checkBox1.Location = new System.Drawing.Point(30, 44);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(104, 29);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "DN 20";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(43, 101);
+            this.checkBox2.Location = new System.Drawing.Point(30, 103);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(104, 29);
             this.checkBox2.TabIndex = 0;
             this.checkBox2.Text = "DN 25";
             this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
             // 
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(43, 160);
+            this.checkBox3.Location = new System.Drawing.Point(30, 162);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(104, 29);
             this.checkBox3.TabIndex = 0;
             this.checkBox3.Text = "DN 32";
             this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
             // 
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(43, 215);
+            this.checkBox4.Location = new System.Drawing.Point(30, 217);
             this.checkBox4.Name = "checkBox4";
             this.checkBox4.Size = new System.Drawing.Size(104, 29);
             this.checkBox4.TabIndex = 0;
             this.checkBox4.Text = "DN 40";
             this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
             // 
             // checkBox5
             // 
             this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(43, 274);
+            this.checkBox5.Location = new System.Drawing.Point(30, 276);
             this.checkBox5.Name = "checkBox5";
             this.checkBox5.Size = new System.Drawing.Size(104, 29);
             this.checkBox5.TabIndex = 0;
             this.checkBox5.Text = "DN 50";
             this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
             // 
             // checkBox6
             // 
             this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(43, 333);
+            this.checkBox6.Location = new System.Drawing.Point(30, 335);
             this.checkBox6.Name = "checkBox6";
             this.checkBox6.Size = new System.Drawing.Size(104, 29);
             this.checkBox6.TabIndex = 0;
             this.checkBox6.Text = "DN 65";
             this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(174, 44);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(104, 29);
+            this.checkBox7.TabIndex = 0;
+            this.checkBox7.Text = "DN 80";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(174, 105);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(116, 29);
+            this.checkBox8.TabIndex = 0;
+            this.checkBox8.Text = "DN 100";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(174, 160);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(116, 29);
+            this.checkBox9.TabIndex = 0;
+            this.checkBox9.Text = "DN 125";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            this.checkBox9.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(174, 219);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(116, 29);
+            this.checkBox10.TabIndex = 0;
+            this.checkBox10.Text = "DN 150";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            this.checkBox10.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(174, 276);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(116, 29);
+            this.checkBox11.TabIndex = 0;
+            this.checkBox11.Text = "DN 200";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            this.checkBox11.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(174, 335);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(116, 29);
+            this.checkBox12.TabIndex = 0;
+            this.checkBox12.Text = "DN 250";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            this.checkBox12.Click += new System.EventHandler(this.dnButtonCheckBox_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox7);
+            this.groupBox1.Controls.Add(this.checkBox6);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox12);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox11);
+            this.groupBox1.Controls.Add(this.checkBox8);
+            this.groupBox1.Controls.Add(this.checkBox10);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox9);
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(315, 403);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rør";
             // 
             // TwinPalette
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.Controls.Add(this.checkBox6);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox5);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "TwinPalette";
             this.Size = new System.Drawing.Size(599, 1130);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -125,5 +222,12 @@
         private System.Windows.Forms.CheckBox checkBox4;
         private System.Windows.Forms.CheckBox checkBox5;
         private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- Acad-C3D-Tools/DriPaletteSet/TwinPalette.cs
+++ Acad-C3D-Tools/DriPaletteSet/TwinPalette.cs
@@ -1,22 +1,73 @@
-﻿using System;
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
 using System.Windows.Forms;
+using IntersectUtilities.UtilsCommon;
+using static IntersectUtilities.UtilsCommon.Utils;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
 
 namespace DriPaletteSet
 {
     public partial class TwinPalette : System.Windows.Forms.UserControl
     {
+        Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+
+        HashSet<CheckBox> dnButtonsCol = new HashSet<CheckBox>();
+
         public TwinPalette()
         {
             InitializeComponent();
 
-            //checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            #region Change appearance
+            checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox2.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox3.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox4.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox5.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox6.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox7.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox8.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox9.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox10.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox11.Appearance = System.Windows.Forms.Appearance.Button;
+            checkBox12.Appearance = System.Windows.Forms.Appearance.Button;
+            #endregion
+
+            #region Add dn buttons to buttons collection
+            dnButtonsCol.Add(checkBox1);
+            dnButtonsCol.Add(checkBox2);
+            dnButtonsCol.Add(checkBox3);
+            dnButtonsCol.Add(checkBox4);
+            dnButtonsCol.Add(checkBox5);
+            dnButtonsCol.Add(checkBox6);
+            dnButtonsCol.Add(checkBox7);
+            dnButtonsCol.Add(checkBox8);
+            dnButtonsCol.Add(checkBox9);
+            dnButtonsCol.Add(checkBox10);
+            dnButtonsCol.Add(checkBox11);
+            dnButtonsCol.Add(checkBox12);
+            #endregion
+        }
+
+        private void dnButtonCheckBox_Click(object sender, EventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            foreach (CheckBox checkBox in dnButtonsCol)
+            {
+                if (checkBox.Checked && cb.Name != checkBox.Name)
+                {
+                    checkBox.Checked = false;
+                }
+            }
         }
     }
 }
