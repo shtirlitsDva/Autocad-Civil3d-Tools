fdabe0425fd942bad8642240727706f1340bb232
MaltheNørlem <man@damgaard-ri.dk>
2022-03-29 08:51:39 +0200

    Added new layer to polydir overrule

--- Acad-C3D-Tools/AcadOverrules/01 PolylineDirection.cs
+++ Acad-C3D-Tools/AcadOverrules/01 PolylineDirection.cs
@@ -56,7 +56,8 @@ namespace AcadOverrules
             if (overruledSubject is Polyline pline)
             {
                 if (pline.Database == null) return false;
-                if (!pline.Layer.StartsWith("0-FJV_fremtid")) return false;
+                if (!(pline.Layer.StartsWith("0-FJV_fremtid") ||
+                      pline.Layer.StartsWith("0-FJV_eks_dim"))) return false;
                 if (pline.NumberOfVertices < 2) return false;
                 if (pline.Length < .1) return false;
                 return true;
