da8976d14d042e623cb4b687d1072a6059309350
Michail Golubjev <xopkop@gmail.com>
2022-05-21 22:33:25 +0200

    Added new utility

---
+++ Acad-C3D-Tools/AutoCADCommands/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoCADCommands")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("luanshixia")]
+[assembly: AssemblyProduct("AutoCADCommands")]
+[assembly: AssemblyCopyright("Copyright © luanshixia")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c6783d73-7c70-455d-a952-6ee574581a94")]
+
+[assembly: AssemblyVersion("2020.0.0.0")]
+[assembly: AssemblyFileVersion("2020.0.0.0")]
