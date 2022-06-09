7707a2639cc5181f260c24cbc36bc61f555a382d
Michail Golubjev <xopkop@gmail.com>
2022-06-09 09:10:04 +0200

    added to gasqa

--- Acad-C3D-Tools/IntersectUtilities/DataQAClass.cs
+++ Acad-C3D-Tools/IntersectUtilities/DataQAClass.cs
@@ -13,13 +13,22 @@ namespace IntersectUtilities.UtilsCommon
             public static HashSet<string> ForbiddenValues = new HashSet<string>()
             {
                 "SÆNKET",
+                "ALLE STIK = Ø40 PM",
                 "ANV. SOM TRÆKRØR",
                 "ANVENDT SOM TRÆKRØR",
+                "BELIGGENHED USIKKER",
+                "I GAMMELT STIK",
+                "M/R G40/65",
+                "MÅSKE RELINET",
+                "SKAB RS 830",
                 "FRITST.M/R SKAB",
                 "G10",
                 "M/R SKAB",
                 "T=0.2",
                 "T=0.3",
+                "T=0.4",
+                "T=0.7",
+                "TERRASSE",
                 "TYPE G65",
                 "SÆNKET 40 CM",
                 "FRIT R-SKAB",
@@ -37,7 +46,8 @@ namespace IntersectUtilities.UtilsCommon
                 { "40 PM 20 MBAR", "40 PM" },
                 { "63 PM 026", "63 PM" },
                 { "63 PM 50 MB", "63 PM" },
-                { "90 PM 50 MBAR", "63 PM" },
+                { "90 PM 50 MBAR", "90 PM" },
+                { "90 PM 0.1", "90 PM" },
                 { "75 ST/63 PM 026", "75 ST" }
             };
         }
