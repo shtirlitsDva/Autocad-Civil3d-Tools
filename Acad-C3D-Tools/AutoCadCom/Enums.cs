8c28aa34142b26425ddf689f34cbdc483934fcb2
Michail Golubjev <xopkop@gmail.com>
2022-12-20 22:38:02 +0100

    Testing AutoCad COM

---
+++ Acad-C3D-Tools/AutoCadCom/Enums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.Interop;
+
+using static AutoCadCom.Enums;
+
+namespace AutoCadCom
+{
+    public static class Enums
+    {
+        public enum AcadStateEnum
+        {
+            Busy,
+            Idle
+        }
+    }
+
+    public static class Tools
+    {
+        public static AcadStateEnum GetAcadState(AcadApplication app)
+        {
+            try
+            {
+                if (app.GetAcadState().IsQuiescent) return AcadStateEnum.Idle;
+                else return AcadStateEnum.Busy;
+            }
+            catch (COMException ex)
+            {
+                //Console.WriteLine($"0x{ex.ErrorCode:X}");
+                //Console.WriteLine($"0x{ex.HResult:X}");
+                //Console.WriteLine(ex.Message);
+                return AcadStateEnum.Busy;
+            }
+        }
+    }
+}
