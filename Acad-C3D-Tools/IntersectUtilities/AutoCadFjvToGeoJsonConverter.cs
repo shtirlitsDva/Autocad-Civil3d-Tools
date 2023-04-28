d117cd0a6dbb0214c9b82b064c3a7718d2eb4849
Michail Golubjev <xopkop@gmail.com>
2023-04-28 23:59:56 +0200

    working on new GeoJson exporter

---
+++ Acad-C3D-Tools/IntersectUtilities/AutoCadFjvToGeoJsonConverter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Civil.DatabaseServices.Styles;
+using Autodesk.Civil.DataShortcuts;
+using Autodesk.Gis.Map;
+using Autodesk.Gis.Map.ObjectData;
+using Autodesk.Gis.Map.Utilities;
+using Autodesk.Aec.PropertyData;
+using Autodesk.Aec.PropertyData.DatabaseServices;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Data;
+using MoreLinq;
+using GroupByCluster;
+using IntersectUtilities.UtilsCommon;
+using static IntersectUtilities.UtilsCommon.Utils;
+using Dreambuild.AutoCAD;
+using FolderSelect;
+
+using static IntersectUtilities.Enums;
+using static IntersectUtilities.HelperMethods;
+using static IntersectUtilities.Utils;
+using static IntersectUtilities.PipeSchedule;
+
+using static IntersectUtilities.UtilsCommon.UtilsDataTables;
+using static IntersectUtilities.UtilsCommon.UtilsODData;
+
+using BlockReference = Autodesk.AutoCAD.DatabaseServices.BlockReference;
+using CivSurface = Autodesk.Civil.DatabaseServices.Surface;
+using DataType = Autodesk.Gis.Map.Constants.DataType;
+using Entity = Autodesk.AutoCAD.DatabaseServices.Entity;
+using ObjectIdCollection = Autodesk.AutoCAD.DatabaseServices.ObjectIdCollection;
+using Oid = Autodesk.AutoCAD.DatabaseServices.ObjectId;
+using OpenMode = Autodesk.AutoCAD.DatabaseServices.OpenMode;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using Label = Autodesk.Civil.DatabaseServices.Label;
+using DBObject = Autodesk.AutoCAD.DatabaseServices.DBObject;
+using IntersectUtilities.DynamicBlocks;
+
+using Autodesk.Aec.DatabaseServices;
+
+namespace IntersectUtilities
+{
+    public interface IAutoCadFjvToGeoJsonConverter
+    {
+        GeoJsonFeature Convert(Entity entity);
+    }
+
+    public class PolylineToGeoJsonConverter : IAutoCadFjvToGeoJsonConverter
+    {
+        public GeoJsonFeature Convert(Entity entity)
+        {
+            if (!(entity is Polyline))
+                throw new ArgumentException($"Entity {entity.Handle} is not a polyline!");
+
+
+        }
+    }
+}
