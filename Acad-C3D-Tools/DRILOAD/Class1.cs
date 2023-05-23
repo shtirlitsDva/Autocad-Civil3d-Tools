2cc09e684de6e0f3f84bbd7f0a119272535ef413
Michail Golubjev <mgo@damgaard-ri.dk>
2023-05-23 21:33:58 +0200

    etc...

---
+++ Acad-C3D-Tools/DRILOAD/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+
+namespace DRILOAD
+{
+    public partial class DriLoad : IExtensionApplication
+    {
+        #region IExtensionApplication members
+        public void Initialize()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            doc.Editor.WriteMessage("\nUse DRILOAD to load DRI programs!");
+        }
+
+        public void Terminate()
+        {
+        }
+        #endregion
+
+        [CommandMethod("DRILOAD")]
+        public void DRILOAD()
+        {
+
+        }
+    }
+}
+
