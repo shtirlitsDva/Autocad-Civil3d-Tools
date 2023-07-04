b69b769a9497c487ca21aae87b0784962382541c
shtirlitsDva <xopkop@gmail.com>
2023-07-04 13:37:33 +0200

    begin to add ler2.0 functionality to aid the processing

---
+++ Acad-C3D-Tools/IntersectUtilities/LER2.0/LER2.0.cs
@@ -0,0 +1,118 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Civil.DatabaseServices.Styles;
+using Autodesk.Civil.DataShortcuts;
+using Autodesk.Gis.Map;
+using Autodesk.Gis.Map.ObjectData;
+using Autodesk.Gis.Map.Utilities;
+using Autodesk.Aec.PropertyData;
+using Autodesk.Aec.PropertyData.DatabaseServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Data;
+using MoreLinq;
+using GroupByCluster;
+using IntersectUtilities.UtilsCommon;
+using Dreambuild.AutoCAD;
+
+using static IntersectUtilities.Enums;
+using static IntersectUtilities.HelperMethods;
+using static IntersectUtilities.Utils;
+using static IntersectUtilities.PipeSchedule;
+
+using static IntersectUtilities.UtilsCommon.UtilsDataTables;
+using static IntersectUtilities.UtilsCommon.UtilsODData;
+using static IntersectUtilities.UtilsCommon.Utils;
+
+using BlockReference = Autodesk.AutoCAD.DatabaseServices.BlockReference;
+using CivSurface = Autodesk.Civil.DatabaseServices.Surface;
+using DataType = Autodesk.Gis.Map.Constants.DataType;
+using Entity = Autodesk.AutoCAD.DatabaseServices.Entity;
+using ObjectIdCollection = Autodesk.AutoCAD.DatabaseServices.ObjectIdCollection;
+using Oid = Autodesk.AutoCAD.DatabaseServices.ObjectId;
+using OpenMode = Autodesk.AutoCAD.DatabaseServices.OpenMode;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using Label = Autodesk.Civil.DatabaseServices.Label;
+using DBObject = Autodesk.AutoCAD.DatabaseServices.DBObject;
+using System.Windows.Documents;
+
+namespace IntersectUtilities
+{
+    public partial class Intersect
+    {
+        [CommandMethod("LER2SORT2DFROM3D")]
+        public void ler2sort2dfrom3d()
+        {
+
+            DocumentCollection docCol = Application.DocumentManager;
+            Database localDb = docCol.MdiActiveDocument.Database;
+            Editor editor = docCol.MdiActiveDocument.Editor;
+
+            using (Transaction tx = localDb.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    ///////////////////////////
+                    bool atZero(double value) => value > -0.0001 && value < 0.0001;
+                    bool at99(double value) => value < -98.0;
+                    bool is3D(double value) => !atZero(value) && !at99(value);
+                    bool is2D(double value) => atZero(value) || at99(value);
+                    ///////////////////////////
+
+                    HashSet<Polyline3d> pls = localDb.HashSetOfType<Polyline3d>(tx, true);
+                    foreach (Polyline3d pl in pls)
+                    {
+                        PolylineVertex3d[] vertices = pl.GetVertices(tx);
+                        HashSet<double> elevs = new HashSet<double>();
+                        for (int i = 0; i < vertices.Length; i++) elevs.Add(vertices[i].Position.Z);
+
+                        if (elevs.All(x => is2D(x)))
+                        {
+                            for (int i = 0; i < vertices.Length; i++)
+                            {
+                                PolylineVertex3d vert = vertices[i];
+                                vert.CheckOrOpenForWrite();
+                                vert.Position =
+                                    new Point3d(
+                                        vert.Position.X, vert.Position.Y, -99.0);
+                            }
+
+                            //Handle the layer name
+                            string currentLayerName = pl.Layer;
+                            if (currentLayerName.Contains("-3D"))
+                            {
+                                string newLayerName = currentLayerName.Replace("-3D", "");
+                                localDb.CheckOrCreateLayer(newLayerName);
+
+                                pl.CheckOrOpenForWrite();
+                                pl.Layer = newLayerName;
+                            }
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    tx.Abort();
+                    prdDbg(ex);
+                    return;
+                }
+                tx.Commit();
+            }
+        }
+    }
+}
