83e888e9276e870c34c50418cbebc8c15a50cb76
shtirlitsDva <xopkop@gmail.com>
2023-07-14 22:40:00 +0200

    start working

---
+++ Acad-C3D-Tools/DRITBL/DRITBL.cs
@@ -0,0 +1,77 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Civil.DatabaseServices.Styles;
+using Autodesk.Civil.DataShortcuts;
+using Autodesk.Aec.PropertyData;
+using Autodesk.Aec.PropertyData.DatabaseServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using MoreLinq;
+//using GroupByCluster;
+using IntersectUtilities.UtilsCommon;
+//using Microsoft.Office.Interop.Excel;
+
+//using static IntersectUtilities.Enums;
+//using static IntersectUtilities.HelperMethods;
+//using static IntersectUtilities.Utils;
+using static IntersectUtilities.UtilsCommon.Utils;
+
+using static IntersectUtilities.UtilsCommon.UtilsDataTables;
+using static IntersectUtilities.UtilsCommon.UtilsODData;
+
+using BlockReference = Autodesk.AutoCAD.DatabaseServices.BlockReference;
+using Entity = Autodesk.AutoCAD.DatabaseServices.Entity;
+using ObjectIdCollection = Autodesk.AutoCAD.DatabaseServices.ObjectIdCollection;
+using Oid = Autodesk.AutoCAD.DatabaseServices.ObjectId;
+using OpenMode = Autodesk.AutoCAD.DatabaseServices.OpenMode;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using DBObject = Autodesk.AutoCAD.DatabaseServices.DBObject;
+using Line = Autodesk.AutoCAD.DatabaseServices.Line;
+
+namespace IntersectUtilities.DRITBL
+{
+    public class DimensioneringExtension : IExtensionApplication
+    {
+        #region IExtensionApplication members
+        public void Initialize()
+        {
+            //Document doc = Application.DocumentManager.MdiActiveDocument;
+            //if (doc != null)
+            //{
+            //    SystemObjects.DynamicLinker.LoadModule(
+            //        "AcMPolygonObj" + Application.Version.Major + ".dbx", false, false);
+            //}
+
+        }
+
+        public void Terminate()
+        {
+        }
+        #endregion
+        [CommandMethod("TBLCHECKPOLYOVERLAP")]
+        public void tblcheckpolyoverlap()
+        {
+
+
+        }
+    }
+}
