df0826f1d195967a006a877e259df0a7e0f2e04b
shtirlitsDva <xopkop@gmail.com>
2023-10-05 12:17:10 +0200

    opensaveclose

--- Acad-C3D-Tools/IntersectUtilities/UtilsCommon.cs
+++ Acad-C3D-Tools/IntersectUtilities/UtilsCommon.cs
@@ -3048,6 +3048,33 @@ namespace IntersectUtilities.UtilsCommon
         }
         public static double ToDegrees(this double radians) => (180 / Math.PI) * radians;
         public static double ToRadians(this double degrees) => (Math.PI / 180) * degrees;
+        public static void Zoom(this Editor ed, Extents3d ext)
+        {
+            if (ed == null)
+                throw new ArgumentNullException("ed");
+            using (ViewTableRecord view = ed.GetCurrentView())
+            {
+                Matrix3d worldToEye = Matrix3d.WorldToPlane(view.ViewDirection) *
+                    Matrix3d.Displacement(Point3d.Origin - view.Target) *
+                    Matrix3d.Rotation(view.ViewTwist, view.ViewDirection, view.Target);
+                ext.TransformBy(worldToEye);
+                view.Width = ext.MaxPoint.X - ext.MinPoint.X;
+                view.Height = ext.MaxPoint.Y - ext.MinPoint.Y;
+                view.CenterPoint = new Point2d(
+                    (ext.MaxPoint.X + ext.MinPoint.X) / 2.0,
+                    (ext.MaxPoint.Y + ext.MinPoint.Y) / 2.0);
+                ed.SetCurrentView(view);
+            }
+        }
+        public static void ZoomExtents(this Editor ed)
+        {
+            Database db = ed.Document.Database;
+            db.UpdateExt(false);
+            Extents3d ext = (short)Application.GetSystemVariable("cvport") == 1 ?
+                new Extents3d(db.Pextmin, db.Pextmax) :
+                new Extents3d(db.Extmin, db.Extmax);
+            ed.Zoom(ext);
+        }
     }
     public class PointDBHorizontalComparer : IEqualityComparer<DBPoint>
     {
