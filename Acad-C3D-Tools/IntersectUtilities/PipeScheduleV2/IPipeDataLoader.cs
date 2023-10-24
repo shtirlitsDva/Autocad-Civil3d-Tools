4d09cb2820352b40c03e6a78b3eaa8f55358dcc7
shtirlitsDva <xopkop@gmail.com>
2023-10-24 16:43:41 +0200

    starting on new pipeschedule

---
+++ Acad-C3D-Tools/IntersectUtilities/PipeScheduleV2/IPipeDataLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectUtilities.PipeScheduleV2
+{
+    public interface IPipeDataLoader
+    {
+        void Load(IEnumerable<string> paths);
+    }
+}
---
+++ Acad-C3D-Tools/IntersectUtilities/PipeScheduleV2/IPipeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectUtilities.PipeScheduleV2
+{
+    public interface IPipeRepository
+    {
+        void Initialize(string type, DataTable pipeTypes);
+        //PipeType GetPipeType(string layerName);
+    }
+}
