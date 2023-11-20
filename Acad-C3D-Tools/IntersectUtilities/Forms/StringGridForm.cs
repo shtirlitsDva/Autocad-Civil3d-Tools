464222f7f826906ddc89412aefeec14e8b9d1286
shtirlitsDva <xopkop@gmail.com>
2023-11-20 12:20:06 +0100

    fixing some dependencies

--- Acad-C3D-Tools/IntersectUtilities/Forms/StringGridForm.cs
+++ Acad-C3D-Tools/IntersectUtilities/Forms/StringGridForm.cs
@@ -1,7 +1,4 @@
-﻿using IntersectUtilities.UtilsCommon;
-using static IntersectUtilities.UtilsCommon.Utils;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
