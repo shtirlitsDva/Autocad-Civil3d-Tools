4936cd65f6cb4595ad3a91de21b1206ea24ec94d
shtirlitsDva <xopkop@gmail.com>
2023-11-23 16:59:44 +0100

    working on pipeline analysis method

---
+++ Acad-C3D-Tools/IntersectUtilities/PipelineNetworkSystem/PipelineNetworkCommands.cs
@@ -0,0 +1,83 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Civil.DatabaseServices.Styles;
+using Autodesk.Civil.DataShortcuts;
+using Autodesk.Aec.PropertyData;
+using Autodesk.Aec.PropertyData.DatabaseServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Data;
+using MoreLinq;
+using GroupByCluster;
+using IntersectUtilities.UtilsCommon;
+using Dreambuild.AutoCAD;
+
+using static IntersectUtilities.Enums;
+using static IntersectUtilities.HelperMethods;
+using static IntersectUtilities.Utils;
+using static IntersectUtilities.UtilsCommon.Utils;
+using static IntersectUtilities.PipeScheduleV2.PipeScheduleV2;
+
+using static IntersectUtilities.UtilsCommon.UtilsDataTables;
+using static IntersectUtilities.UtilsCommon.UtilsODData;
+
+using BlockReference = Autodesk.AutoCAD.DatabaseServices.BlockReference;
+using CivSurface = Autodesk.Civil.DatabaseServices.Surface;
+using DataType = Autodesk.Gis.Map.Constants.DataType;
+using Entity = Autodesk.AutoCAD.DatabaseServices.Entity;
+using ObjectIdCollection = Autodesk.AutoCAD.DatabaseServices.ObjectIdCollection;
+using Oid = Autodesk.AutoCAD.DatabaseServices.ObjectId;
+using OpenMode = Autodesk.AutoCAD.DatabaseServices.OpenMode;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using Label = Autodesk.Civil.DatabaseServices.Label;
+using DBObject = Autodesk.AutoCAD.DatabaseServices.DBObject;
+using IntersectUtilities.PipelineNetworkSystem;
+
+namespace IntersectUtilities
+{
+    public partial class Intersect
+    {
+#if DEBUG
+        [CommandMethod("TESTPIPENETWORK")]
+        [CommandMethod("TPN")]
+        public void testpipenetwork()
+        {
+            DocumentCollection docCol = Application.DocumentManager;
+            Database localDb = docCol.MdiActiveDocument.Database;
+            
+            using (Transaction tx = localDb.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    PipelineNetwork pn = new PipelineNetwork();
+                    pn.CreatePipeNetwork(localDb);
+
+
+                }
+                catch (System.Exception ex)
+                {
+                    tx.Abort();
+                    prdDbg(ex);
+                    return;
+                }
+                tx.Commit();
+            }
+        }
+#endif
+    }
+}
