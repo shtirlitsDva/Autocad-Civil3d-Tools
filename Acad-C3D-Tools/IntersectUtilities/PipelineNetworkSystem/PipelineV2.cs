8f4c623c7750ec7afe58b7158d361765b927a124
shtirlitsDva <xopkop@gmail.com>
2023-12-07 16:19:15 +0100

    trying to make it work

--- Acad-C3D-Tools/IntersectUtilities/PipelineNetworkSystem/PipelineV2.cs
+++ Acad-C3D-Tools/IntersectUtilities/PipelineNetworkSystem/PipelineV2.cs
@@ -44,8 +44,8 @@ namespace IntersectUtilities.PipelineNetworkSystem
         {
             source.Partition(IsNotWeld, out this.ents, out this.welds);
 
-            bool IsNotWeld(Entity e) => 
-                (e is BlockReference br && 
+            bool IsNotWeld(Entity e) =>
+                (e is BlockReference br &&
                 br.ReadDynamicCsvProperty(
                     DynamicProperty.Type, CsvData.Get("fjvKomponenter"), false) != "Svejsning") ||
                     e is Polyline; //<-- this is the culprit!!!!!!!!!!!!!!!!!!!!
@@ -97,31 +97,59 @@ namespace IntersectUtilities.PipelineNetworkSystem
                         }
                     }
                 }
+                else if (sizes.Arrangement == PipelineSizeArray.PipelineSizesArrangement.MiddleDescendingToEnds)
+                    throw new Exception("MiddleDescendingToEnds for a root pipeline is not supported!");
                 else
                 {
-                    if (!(parent is PipelineV2Alignment))
-                        throw new Exception(
-                            $"Parent {parent.Name} is not an alignment pipeline, but is {parent.GetType().Name}!\n" +
-                            $"This is not supported.");
+                    switch (sizes.Arrangement)
+                    {
+                        case PipelineSizeArray.PipelineSizesArrangement.SmallToLargeAscending:
 
-                    var pal = parent as PipelineV2Alignment;
+                            break;
+                        case PipelineSizeArray.PipelineSizesArrangement.LargeToSmallDescending:
+                            break;
+                        default:
+                            throw new Exception($"{sizes.Arrangement} should be handled elsewhere!");    
+                    }
                 }
-                
             }
             else
             {
+                if (!(parent is PipelineV2Alignment))
+                    throw new Exception(
+                        $"Parent {parent.Name} is not an alignment pipeline, but is {parent.GetType().Name}!\n" +
+                        $"This is not supported.");
+
                 if (sizes.Arrangement == PipelineSizeArray.PipelineSizesArrangement.MiddleDescendingToEnds)
                 {
 
                 }
                 else
                 {
+                    var pal = parent as PipelineV2Alignment;
 
                 }
             }
 
             refPline.UpgradeOpen();
             refPline.Erase(true);
+        }
+
+        private bool PointIsOn(Alignment al, Point3d point, double tol)
+        {
+            Polyline pline = al.GetPolyline().Go<Polyline>(
+                al.Database.TransactionManager.TopTransaction);
+
+            Point3d p = pline.GetClosestPointTo(point, false);
+            pline.UpgradeOpen();
+            pline.Erase(true);
+            //prdDbg($"{offset}, {Math.Abs(offset)} < {tolerance}, {Math.Abs(offset) <= tolerance}, {station}");
+
+            // If the offset is within the tolerance, the point is on the alignment
+            if (Math.Abs(p.DistanceTo(point)) <= tol) return true;
+
+            // Otherwise, the point is not on the alignment
+            return false;
         }
 
         public override bool IsConnectedTo(IPipelineV2 other, double tol)
@@ -139,7 +167,7 @@ namespace IntersectUtilities.PipelineNetworkSystem
     }
     public class PipelineV2Na : PipelineV2Base
     {
-        public PipelineV2Na(IEnumerable<Entity> ents) : base(ents) {}
+        public PipelineV2Na(IEnumerable<Entity> ents) : base(ents) { }
         public override string Name =>
             psh.Pipeline.ReadPropertyString(
                 this.Entities.First(), psh.PipelineDef.BelongsToAlignment);
