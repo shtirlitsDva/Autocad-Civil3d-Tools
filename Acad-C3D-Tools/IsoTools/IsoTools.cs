0bac592001b56a93592af47c9afce5e6e5ee54d6
shtirlitsDva <xopkop@gmail.com>
2024-06-11 11:21:46 +0200

    finished processing reports script

--- Acad-C3D-Tools/IsoTools/IsoTools.cs
+++ Acad-C3D-Tools/IsoTools/IsoTools.cs
@@ -49,9 +49,9 @@ namespace IsoTools
                     #region Start processing
                     var brefs = localDb.GetBlockReferenceByName("Title Block");
                     if (brefs.Count == 0) throw new System.Exception("No block named Title Block found!");
-                    var br = brefs.FirstOrDefault();
+                    var localTb = brefs.FirstOrDefault();
 
-                    string pipelineName = br.GetAttributeStringValue("LINE_NAME");
+                    string pipelineName = localTb.GetAttributeStringValue("LINE_NAME");
                     string drawingDir = Path.GetDirectoryName(localDb.Filename);
                     //Create reference to the reports directory
                     var reportsDir = Path.Combine(Directory.GetParent(drawingDir).FullName, "Reports");
@@ -63,15 +63,41 @@ namespace IsoTools
                     Encoding encoding = DetectFileEncoding(matFile);
 
                     var cws = new[] { 6, 15, 52, 20 };
-                    
+
                     var data = ConvertFixedWidthToCollection(matFile, cws);
-                    //foreach (var item in data) prdDbg(string.Join("|", item));
-                    var hdngs = data[0];
-                    data.RemoveAt(0);
+                    //Handle the M in the last column
+                    foreach (var item in data)
+                    {
+                        if (item.Last().Contains("M"))
+                        {
+                            var split = item.Last().Split(' ');
+                            item[item.Count - 1] = split[0];
+                            item.Add(split[1]);
+                        }
+                    }
 
+                    File.WriteAllLines(
+                        Path.Combine(reportsDir, Path.GetFileNameWithoutExtension(matFile) + ".csv"),
+                        data.Select(x => string.Join(";", x)),
+                        new UTF8Encoding(true)
+                        );
+                    #endregion
 
+                    #region Process Cutlist
+                    string cutFile = Path.Combine(reportsDir, pipelineName + "-CUT_LIST.txt");
+                    if (!File.Exists(cutFile)) throw new System.Exception("Cut list file not found!");
 
+                    encoding = DetectFileEncoding(cutFile);
 
+                    cws = new[] { 5, 9, 11, 100 };
+
+                    data = ConvertFixedWidthToCollection(cutFile, cws);
+
+                    File.WriteAllLines(
+                        Path.Combine(reportsDir, Path.GetFileNameWithoutExtension(cutFile) + ".csv"),
+                        data.Select(x => string.Join(";", x)),
+                        new UTF8Encoding(true)
+                        );
                     #endregion
 
                     #region Process welds
@@ -79,18 +105,144 @@ namespace IsoTools
                     if (!File.Exists(drawingXml)) throw new System.Exception("Drawing XML file not found!");
 
                     XDocument xmlDoc = XDocument.Load(drawingXml);
-                    var outputFiles = xmlDoc.Descendants("OUTPUT-FILE").Select(node => node.Value.Trim());
+                    var outputFiles = xmlDoc.Descendants("OUTPUT-FILE")
+                        .Select(node => node.Value.Trim()).ToList();
 
-                    //foreach (string file in outputFiles) prdDbg(file);
+                    //Remove the open file from the list
+                    outputFiles.RemoveAt(outputFiles.IndexOf(localDb.Filename));
+
+                    //Gather the data about the blocks and their UCIs
+                    //Start with localDb
+                    var blockUcis = new Dictionary<string, string>();
+
+                    #region Populate blockUcis by reading local and external files
+                    //Determine blad number
+                    var localTexts = localDb.HashSetOfType<DBText>(tx);
+                    string localBlad = "";
+                    foreach (var txt in localTexts)
+                    {
+                        if ((int)txt.Position.X == 395 && (int)txt.Position.Y == 17)
+                        {
+                            prdDbg($"Blad {txt.TextString} text found!");
+                            localBlad = txt.TextString;
+                        }
+                    }
+                    if (localBlad == "")
+                        throw new System.Exception($"Blad number could not be determined for file: " +
+                            $"{Path.GetFileName(localDb.Filename)}!");
+
+                    //Cache all UCIS and blad numbers
+                    var localBrs = localDb.HashSetOfType<BlockReference>(tx);
+                    foreach (var br in localBrs)
+                    {
+                        try
+                        {
+                            if (br.GetAttributeStringValue("UCI").IsNoE()) continue;
+                            blockUcis.Add(
+                                br.GetAttributeStringValue("UCI"), localBlad);
+                        }
+                        catch (System.Exception)
+                        {
+                            prdDbg($"UCI {br.GetAttributeStringValue("UCI")} alrady exists!");
+                            throw;
+                        }
+                    }
+
+                    //Process the rest of the files
+                    foreach (var file in outputFiles)
+                    {
+                        var db = new Database(false, true);
+                        db.ReadDwgFile(file, FileOpenMode.OpenForReadAndAllShare, false, "");
+
+                        using (Transaction t = db.TransactionManager.StartTransaction())
+                        {
+                            try
+                            {
+                                string blad = "";
+                                var texts = db.HashSetOfType<DBText>(t);
+                                foreach (var txt in texts)
+                                {
+                                    if ((int)txt.Position.X == 395 && (int)txt.Position.Y == 17)
+                                    {
+                                        prdDbg($"Blad {txt.TextString} text found!");
+                                        blad = txt.TextString;
+                                    }
+                                }
+                                if (blad == "")
+                                    throw new System.Exception($"Blad number could not be determined for file: " +
+                                        $"{Path.GetFileName(localDb.Filename)}!");
+
+                                var brs = db.HashSetOfType<BlockReference>(t);
+                                foreach (var br in brs)
+                                {
+                                    if (br.GetAttributeStringValue("UCI").IsNoE()) continue;
+                                    blockUcis.Add(
+                                        br.GetAttributeStringValue("UCI"), blad);
+                                }
+                            }
+                            catch (System.Exception)
+                            {
+                                prdDbg($"Processing failed for file {Path.GetFileName(file)}!");
+                                t.Abort();
+                                t.Dispose();
+                                db.Dispose();
+                                throw;
+                            }
+                            t.Abort();
+                        }
+                        db.Dispose();
+                    }
+                    #endregion
+
+                    //Now process the welds text file
+                    string wldFile = Path.Combine(reportsDir, pipelineName + "-WELD_LIST.txt");
+                    if (!File.Exists(wldFile)) throw new System.Exception("Weld list file not found!");
+
+                    encoding = DetectFileEncoding(wldFile);
+
+                    cws = new[] { 10, 10, 100 };
+
+                    data = ConvertFixedWidthToCollection(wldFile, cws);
+
+                    data[0][2] = "Tegning";
+
+                    foreach (var d in data.Skip(1))
+                    {
+                        var uci = d[2];
+                        if (blockUcis.ContainsKey(uci))
+                        {
+                            d[2] = pipelineName + " Blad " + blockUcis[uci];
+                        }
+                        else prdDbg($"Weld UCI {uci} not found in drawings!");
+                    }
+
+                    SortListAsIntsKeepingHeader(data);
+
+                    File.WriteAllLines(
+                        Path.Combine(reportsDir, Path.GetFileNameWithoutExtension(wldFile) + ".csv"),
+                        data.Select(x => string.Join(";", x)),
+                        new UTF8Encoding(true)
+                        );
                     #endregion
 
                     #region Utils
-                    List<string[]> ConvertFixedWidthToCollection(string inputFilePath, int[] widths)
+                    void SortListAsIntsKeepingHeader(List<List<string>> lists)
                     {
-                        var records = new List<string[]>();
+                        if (lists.Count > 1)
+                        {
+                            var sorted = lists.Skip(1)
+                                .OrderBy(list => int.Parse(list[0]))
+                                .ToList();
+                            lists.RemoveRange(1, lists.Count - 1);
+                            lists.AddRange(sorted);
+                        }
+                    }
+                    List<List<string>> ConvertFixedWidthToCollection(string inputFilePath, int[] widths)
+                    {
+                        var records = new List<List<string>>();
                         var lines = File.ReadAllLines(inputFilePath);
 
-                        prdDbg(string.Join(", ", lines.Select(x => x.Length)));
+                        //prdDbg(string.Join(", ", lines.Select(x => x.Length)));
 
                         foreach (var line in lines)
                         {
@@ -113,12 +265,11 @@ namespace IsoTools
                                 }
                             }
 
-                            records.Add(record.ToArray());
+                            records.Add(record);
                         }
 
                         return records;
                     }
-
                     Encoding DetectFileEncoding(string filePath)
                     {
                         using (FileStream fs = File.OpenRead(filePath))
