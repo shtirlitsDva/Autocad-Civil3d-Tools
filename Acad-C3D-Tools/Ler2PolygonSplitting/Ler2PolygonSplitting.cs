ba7d53fbbe602fddc10e924635edd4c77a95656f
Michail Golubjev <xopkop@gmail.com>
2024-08-11 18:11:08 +0200

    one project at a time

--- Acad-C3D-Tools/Ler2PolygonSplitting/Ler2PolygonSplitting.cs
+++ Acad-C3D-Tools/Ler2PolygonSplitting/Ler2PolygonSplitting.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Autodesk.AutoCAD.ApplicationServices.Core;
 using Autodesk.AutoCAD.EditorInput;
 using System.IO;
 using System.Reflection;
