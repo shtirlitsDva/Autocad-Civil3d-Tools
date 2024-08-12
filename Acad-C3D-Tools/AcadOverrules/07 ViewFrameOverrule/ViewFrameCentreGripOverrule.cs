46d2d6836fcb8d0960cb46503f8a2ed2549665fc
Michail Golubjev <xopkop@gmail.com>
2024-08-12 21:16:38 +0200

    changes

---
+++ Acad-C3D-Tools/AcadOverrules/07 ViewFrameOverrule/ViewFrameCentreGripOverrule.cs
@@ -0,0 +1,78 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcadOverrules.ViewFrameGripOverrule
+{
+    internal class ViewFrameCentreGripOverrule : GripOverrule
+    {
+        private bool _enabled = false;
+        private bool _originalOverruling = false;
+        private static ViewFrameCentreGripOverrule _instance = null;
+        private static string _layerName = "";
+        public static ViewFrameCentreGripOverrule Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ViewFrameCentreGripOverrule();
+                }
+                return _instance;
+            }
+        }
+        public bool HideOriginals { get; set; } = true;
+        public void EnableOverrule(bool enable)
+        {
+            if (enable)
+            {
+                if (_enabled) return;
+                _originalOverruling = Overrule.Overruling;
+                AddOverrule(RXClass.GetClass(typeof(Polyline)), this, false);
+                SetCustomFilter();
+                Overrule.Overruling = true;
+                _enabled = true;
+            }
+            else
+            {
+                if (!_enabled) return;
+                RemoveOverrule(RXClass.GetClass(typeof(Polyline)), this);
+                Overrule.Overruling = _originalOverruling;
+                _enabled = false;
+            }
+        }
+        public override bool IsApplicable(RXObject overruledSubject)
+        {
+            var poly = overruledSubject as Polyline;
+            if (poly != null)
+            {
+                bool polyClosed = poly.Closed;
+
+
+                return poly.Closed;
+            }
+            return false;
+        }
+        public override void GetGripPoints(
+        Entity entity,
+        GripDataCollection grips,
+        double curViewUnitSize,
+        int gripSize,
+        Vector3d curViewDir,
+        GetGripPointsFlags bitFlags)
+        {
+            var poly = entity as Polyline;
+            if (poly != null && poly.Closed)
+            {
+                using (var tran = entity.Database.TransactionManager.StartTrans
+                action())
+{
+                }
+
+            }
---
+++ Acad-C3D-Tools/AcadOverrules/07 ViewFrameOverrule/ViewFrameGrip.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.GraphicsInterface;
+using Autodesk.AutoCAD.Runtime;
+
+using Oid = Autodesk.AutoCAD.DatabaseServices.ObjectId;
+
+namespace AcadOverrules.ViewFrameGripOverrule
+{
+    internal class ViewFrameGrip : GripData
+    {
+        public ViewFrameGrip()
+        {
+            ForcedPickOn = false;
+            GizmosEnabled = false;
+            DrawAtDragImageGripPoint = false;
+            IsPerViewport = false;
+            ModeKeywordsDisabled = true;
+            RubberBandLineDisabled = true;
+            TriggerGrip = true;
+            HotGripInvokesRightClick = true;
+            HotGripInvokesRightClick = false;
+        }
+        public Oid EntityId { get; set; } = Oid.Null;
+
+        public override bool ViewportDraw(
+            ViewportDraw worldDraw,
+            ObjectId entityId,
+            DrawType type,
+            Point3d? imageGripPoint,
+            int gripSizeInPixels)
+        {
+            var unit = worldDraw.Viewport.GetNumPixelsInUnitSquare(GripPoint);
+            var gripHeight = 3.0 * gripSizeInPixels / unit.X;
+            var points = new Point3dCollection();
+            var x = GripPoint.X;
+            var y = GripPoint.Y;
+            var offset = gripHeight / 2.0;
+            points.Add(new Point3d(x - offset, y, 0.0));
+            points.Add(new Point3d(x, y - offset, 0.0));
+            points.Add(new Point3d(x + offset, y, 0.0));
+            points.Add(new Point3d(x, y + offset, 0.0));
+            Point3d center = new Point3d(x, y, 0.0);
+            var radius = offset;
+            worldDraw.SubEntityTraits.FillType = FillType.FillAlways;
+            worldDraw.SubEntityTraits.Color = 30;
+            worldDraw.Geometry.Circle(center, radius, Vector3d.ZAxis);
+            return true;
+        }
+
+        public override ReturnValue OnHotGrip(Oid entityId, Context contextFlags)
+        {
+            return ReturnValue.Ok;
+        }
+    }
+}
