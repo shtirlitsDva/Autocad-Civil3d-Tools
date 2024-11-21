ae5b9468ad1c2e96cc0a9ba96b2aea3a9d214486
Michail Golubjev <xopkop@gmail.com>
2024-11-21 22:08:13 +0100

    woooooorking

--- Acad-C3D-Tools/Dimensionering/DimensioneringV2/Commands.cs
+++ Acad-C3D-Tools/Dimensionering/DimensioneringV2/Commands.cs
@@ -1,11 +1,24 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
 
+using Newtonsoft.Json;
+
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+
+using IntersectUtilities.UtilsCommon;
+using static IntersectUtilities.UtilsCommon.Utils;
+using System.Windows.Forms;
+
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using Autodesk.AutoCAD.Geometry;
 
 namespace Dimensionering.DimensioneringV2
 {
@@ -30,5 +43,55 @@ namespace Dimensionering.DimensioneringV2
         {
         }
         #endregion
+
+        private double tol = Tolerance.Global.EqualPoint;
+
+        public void dimv2analyzevejmidte()
+        {
+            DocumentCollection docCol = Application.DocumentManager;
+            Database localDb = docCol.MdiActiveDocument.Database;
+            Editor editor = docCol.MdiActiveDocument.Editor;
+            Document doc = docCol.MdiActiveDocument;
+
+            using (Transaction tx = localDb.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    var pls = localDb.HashSetOfType<Polyline>(tx)
+                        .Where(x => x.Layer == "Vejmidte-tændt");
+
+                    foreach (var pl1 in pls)
+                    {
+                        var sp = pl1.StartPoint;
+                        var ep = pl1.EndPoint;
+
+                        foreach (var pl2 in pls)
+                        {
+                            if (pl1 == pl2) continue;
+
+                            if (sp.IsOnCurve(pl2, tol)) addVertexIfMissing(pl2, sp);
+                        }
+                    }
+
+                    void addVertexIfMissing(Polyline pl, Point3d pt)
+                    {
+                        var idx = pl.GetCoincidentIndexAtPoint(pt);
+                        if (idx == -1)
+                        {
+                            
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    prdDbg(ex);
+                    tx.Abort();
+                    return;
+                }
+                tx.Commit();
+            }
+
+            prdDbg("Finished!");
+        }
     }
 }
