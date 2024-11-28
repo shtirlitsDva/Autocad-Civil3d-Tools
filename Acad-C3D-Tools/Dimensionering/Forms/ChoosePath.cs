61414eea6e6144e8523f9b91c97bcc8f7446655e
Michail Golubjev <xopkop@gmail.com>
2024-11-28 19:45:28 +0100

    fix some errors

--- Acad-C3D-Tools/Dimensionering/Forms/ChoosePath.cs
+++ Acad-C3D-Tools/Dimensionering/Forms/ChoosePath.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
-using DimensioneringV2.Properties;
+using Dimensionering.Properties;
 
 namespace IntersectUtilities.Dimensionering.Forms
 {
