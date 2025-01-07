06a2172ef8faf9435d04cf35eb3ccc2b13123890
Michail Golubjev <mgo@norsyn.dk>
2025-01-07 23:51:18 +0100

    small correction

--- Acad-C3D-Tools/IntersectUtilities/PipelineNetworkSystem/QuikGraphClasses/EdgeBase.cs
+++ Acad-C3D-Tools/IntersectUtilities/PipelineNetworkSystem/QuikGraphClasses/EdgeBase.cs
@@ -11,17 +11,15 @@ namespace IntersectUtilities.PipelineNetworkSystem.QuikGraphClasses
 {
     internal class EdgeBase<TNode> : IEdge<TNode> where TNode : NodeBase
     {
-        public TNode _source { get; private set; }
-        public TNode _target { get; private set; }
+        private TNode _source;
+        private TNode _target;
+        public TNode Source => _source;
+        public TNode Target => _target;
 
         public EdgeBase([NotNull] TNode source, [NotNull] TNode target)
         {
             _source = source ?? throw new ArgumentNullException(nameof(source));
             _target = target ?? throw new ArgumentNullException(nameof(target));
         }
-
-        public TNode Source => _source;
-
-        public TNode Target => _target;
     }
 }
