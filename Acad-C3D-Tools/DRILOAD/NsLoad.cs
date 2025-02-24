480525aa74daed7085a1e63c4bd504a53d8022f2
Daniel Marker Oksborg Levisen <DML@norsyn.dk>
2025-02-24 09:35:03 +0100

    add nsloadall

--- Acad-C3D-Tools/DRILOAD/NsLoad.cs
+++ Acad-C3D-Tools/DRILOAD/NsLoad.cs
@@ -69,6 +69,46 @@ namespace DRILOAD
                 $"DLL {System.IO.Path.GetFileName(pathToLoad)} loaded!");
         }
 
+        [CommandMethod("NSLOADALL")]
+        public void nsloadall()
+        {
+            System.Data.DataTable dt = ReadCsvToDataTable(
+                @"X:\AutoCAD DRI - 01 Civil 3D\NetloadV2\Register-2025.csv", "Register");
+
+            Dictionary<string, string> dllDict = new Dictionary<string, string>();
+
+            foreach (System.Data.DataRow row in dt.Rows)
+                dllDict.Add(row["DisplayName"].ToString(), row["Path"].ToString());
+
+            foreach (var dllFile in dllDict)
+            {
+                string pathToLoad = dllFile.Value;
+
+                if (!System.IO.File.Exists(pathToLoad))
+                    throw new System.Exception($"DLL file {pathToLoad} does not exist!");
+
+                try
+                {
+                    Assembly.LoadFrom(pathToLoad);
+                }
+                catch (System.Exception ex)
+                {
+                    Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage(
+                      "\nCannot load {0}: {1}",
+                      pathToLoad,
+                      ex.Message
+                    );
+                    Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage(
+                      "\n" + ex.ToString()
+                    );
+                    return;
+                }
+
+                Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage(
+                    $"DLL {System.IO.Path.GetFileName(pathToLoad)} loaded!");
+            }
+        }
+
         //private static string GetKeywords(string message, ICollection<string> keywords)
         //{
         //    if (keywords.Count == 0) return null;
