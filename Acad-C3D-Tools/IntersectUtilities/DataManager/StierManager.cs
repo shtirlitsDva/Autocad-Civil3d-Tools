12b023e52db6ad78825db43486a6998845ae9a0a
shtirlitsDva <xopkop@gmail.com>
2025-07-08 16:52:38 +0200

    new stuff

---
+++ Acad-C3D-Tools/IntersectUtilities/DataManager/StierManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectUtilities.DataManager
+{
+    internal static class StierManager
+    {
+        private static readonly string[] ValueColumns = ["Ler", "Surface", "Alignments", "Fremtid", "Længdeprofiler"];
+
+        private static readonly Lazy<Dictionary<(string ProjectId, string EtapeId), HashSet<StierRecord>>> _cache
+            = new(() => LoadAndResolve(), isThreadSafe: true);
+    }
+}
---
+++ Acad-C3D-Tools/IntersectUtilities/DataManager/StierRecord.cs
@@ -0,0 +1,4 @@
+﻿namespace IntersectUtilities.DataManager
+{
+    internal readonly record struct StierRecord(string projectId, string etapeId, StierDataType dataType, string filePath);
+}
