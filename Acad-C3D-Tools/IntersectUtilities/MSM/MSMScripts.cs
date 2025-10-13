58f39bc529fdc8dbbed68d076305679eacbe366c
msm <msm@norsyn.dk>
2025-10-13 12:50:37 +0200

    update

--- Acad-C3D-Tools/IntersectUtilities/MSM/MSMScripts.cs
+++ Acad-C3D-Tools/IntersectUtilities/MSM/MSMScripts.cs
@@ -69,7 +69,7 @@ namespace IntersectUtilities
                     {
                         Position = insPt,
                         TextString = pv.Name.Replace("_PV", ""),
-                        Height = 10,
+                        Height = 20,
                         Layer = "0"
                     };
 
