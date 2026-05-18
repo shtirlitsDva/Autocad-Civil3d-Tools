af3935e947022ddf7d83d725460b6d798ef572fd
Martin Petrov <mpe@norsyn.dk>
2026-05-18 12:42:56 +0200

    alupexfix3

--- Acad-C3D-Tools/IntersectUtilities/MPE/PipePlan/PipePlanWidthCalculator.cs
+++ Acad-C3D-Tools/IntersectUtilities/MPE/PipePlan/PipePlanWidthCalculator.cs
@@ -6,7 +6,7 @@ internal static class PipePlanWidthCalculator
 {
     public static double ResolveDrawingWidth(string layerName)
     {
-        NSPaletteAdapter.TryGetCurrentSeries(out PipeSeriesEnum series);
+        PipeSeriesEnum series = PipeSeriesEnum.S2;
         double kOdMillimeters = PipeScheduleV2.PipeScheduleV2.GetPipeKOd(layerName, series);
         return kOdMillimeters > 0.0 ? kOdMillimeters / 1000.0 : 0.0;
     }
